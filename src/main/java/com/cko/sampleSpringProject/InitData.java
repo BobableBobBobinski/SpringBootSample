ba7dbba7b7f2a4b4c544876f1bdf5ebc318e4343
BobableBobBobinski <polinaim888@gmail.com>
2019-12-08 09:45:22 +0300

    Something is wrong with init or model

--- src/main/java/com/cko/sampleSpringProject/InitData.java
+++ src/main/java/com/cko/sampleSpringProject/InitData.java
@@ -1,8 +1,10 @@
 package com.cko.sampleSpringProject;
 
 import com.cko.sampleSpringProject.dao.FilmDAO;
+import com.cko.sampleSpringProject.dao.ProductDAO;
 import com.cko.sampleSpringProject.model.Authority;
 import com.cko.sampleSpringProject.model.Film;
+import com.cko.sampleSpringProject.model.Product;
 import com.cko.sampleSpringProject.model.User;
 import com.cko.sampleSpringProject.service.AuthorityService;
 import com.cko.sampleSpringProject.service.SMSCService;
@@ -35,23 +37,28 @@ public class InitData {
 
     @Autowired
     FilmDAO filmDAO;
+
     public void initData() {
 
 
 //        smscSender.send_sms("89775548911","TEST MESSAGE",1, "", "", 0, "", "");
         initFilms();
         initUserAndRoles();
-
+        initProducts();
 
     }
+
+    @Autowired
+    ProductDAO productDAO;
+
     private void initFilms() {
         for (int i = 0; i < 10; i++) {
-            Film film = new Film(faker.superhero().name(), i, i*2);
+            Film film = new Film(faker.superhero().name(), i, i * 2);
             filmDAO.save(film);
         }
     }
 
-    private void initUserAndRoles(){
+    private void initUserAndRoles() {
         Authority adminAuthority = new Authority("ROLE_ADMIN");
         Authority userAuthority = new Authority("ROLE_USER");
         authorityService.insert(adminAuthority);
@@ -61,10 +68,17 @@ public class InitData {
         authorities.add(adminAuthority);
 
         for (int i = 0; i < 10; i++) {
-            userService.insert(new User(i+"@mail.ru",bCryptPasswordEncoder.encode(String.valueOf(i)), authorities));
+            userService.insert(new User(i + "@mail.ru", bCryptPasswordEncoder.encode(String.valueOf(i)), authorities));
         }
 
 
+    }
 
+    public void initProducts() {
+
+        for (int j = 0; j < 10; j++) {
+            Product product = new Product(faker.book().title(), faker.number().numberBetween(100, 3500), faker.number().numberBetween(10, 30));
+            productDAO.save(product);
+        }
     }
 }
---
+++ src/main/java/com/cko/sampleSpringProject/controller/ProductController.java
@@ -0,0 +1,62 @@
+package com.cko.sampleSpringProject.controller;
+
+
+import com.cko.sampleSpringProject.dao.FilmDAO;
+import com.cko.sampleSpringProject.dao.ProductDAO;
+import com.cko.sampleSpringProject.model.Film;
+import com.cko.sampleSpringProject.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import java.util.List;
+@Controller
+
+public class ProductController {
+
+    @Autowired
+    ProductDAO productDAO;
+
+    @GetMapping("/products/all")
+        public ModelAndView showAllProductsPage(){
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("allProducts");
+            List<Product> productlist = productDAO.findAll();
+            modelAndView.addObject("products",productlist);
+
+            return modelAndView;
+        }
+
+
+        @GetMapping("/products/create")
+        public String showCreateProductPage() {
+            return "createProduct";
+        }
+
+
+        @GetMapping("/products/edit")
+            public ModelAndView showEditProductPage(@RequestParam Long id){
+                ModelAndView modelAndView = new ModelAndView();
+
+                Product product = productDAO.findProductById(id);
+                modelAndView.addObject("product",product);
+                modelAndView.setViewName("editProduct");
+                return modelAndView;
+            }
+            @PostMapping("/products/edit")
+            public RedirectView editFilm(Product product){
+                productDAO.save(product);
+
+                return new RedirectView("/products/all");
+
+            }
+            @PostMapping("/products/create")
+            public RedirectView createProduct(Product product){
+                productDAO.save(product);
+                return new RedirectView("/products/all");
+            }
+        }
